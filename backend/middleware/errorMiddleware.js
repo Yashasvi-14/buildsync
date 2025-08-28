8848e67d6c90db506a6ae175afd4797c2d434d23
Yashasvi-14 <Yashasvi-14@users.noreply.github.com>
2025-08-28 23:39:30 +0530

    modified: notFouund and error handlers

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,15 +1,20 @@
 //not found handler
-export const notFound = (req,res,next) => {
+const notFound = (req,res,next) => {
+    const error=new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
-    res.json({success: false, message: `Not Found - ${req.originalUrl}`});
+    next(error);
 };
 
 //error handler
-export const errorHandler = (err, req, res, next) => {
-    console.error(err.stack || err);
-    const status = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
-    res.status(status).json({
-        success: false,
-        message: err.message || "Server Error",
-    });
+const errorHandler = (err, req, res, next) => {
+   
+    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    })
+    
 };
+
+export{notFound,errorHandler};
