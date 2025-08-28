5c01512df0dbe6a60f192a7cbf761fda7108f9f9
Yashasvi-14 <Yashasvi-14@users.noreply.github.com>
2025-08-28 19:26:35 +0530

    feat(db): add MongoDB connection helper

---
+++ config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const connrctDB = async () => {
+    const uri = process.env.MONGODB_URI;
+    if(!uri) throw new Error("MONGO_URI not set");
+    try {
+        const conn = await mongoose.connect(uri , {
+
+        });
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error("MongoDB connection error:", err.message);
+        process.exit(1);
+    }
+};
