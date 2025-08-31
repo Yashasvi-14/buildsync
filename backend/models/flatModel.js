4ba616e1e2f495c2839b5359b97433cedc0f604b
Yashasvi-14 <Yashasvi-14@users.noreply.github.com>
2025-08-31 18:53:45 +0530

    feat: Add details in flat data model

---
+++ backend/models/flatModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import Building from "./buildingModel";
+
+const flatSchema = new mongoose.Schema(
+    {
+        flatNumber: {
+            type: String,
+            required: [true, 'Flat number is required'],
+            trim: true,
+        },
+        building: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Building',
+        },
+        resident: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            default: null,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Flat = mongoose.model('Flat', flatSchema);
+
+export default Flat;
