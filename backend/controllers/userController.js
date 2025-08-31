69e0b48eb3db51fc28f683ce8c971a2fee3f3297
Yashasvi-14 <Yashasvi-14@users.noreply.github.com>
2025-08-31 13:09:50 +0530

    feat: Add auth middleware and protected user profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -82,3 +82,21 @@ export const loginUser = async (req,res,next) => {
         next(error);
     }
 };
+
+/**
+ * @desc    Get user profile
+ * @route   GET /api/users/profile
+ * @access  Private
+ */
+export const getUserProfile = async(req, res) => {
+    if(req.user) {
+        res.json({
+            _id: req.user._id,
+            name: req.user.name,
+            email: req.user.email,
+            role: req.user.role,
+        });
+    } else {
+        res.status(404).json({message: 'User not found'});
+    }
+};
