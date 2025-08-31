d1733075ad62a36b5173a8c065a05770e3163abf
Yashasvi-14 <Yashasvi-14@users.noreply.github.com>
2025-08-31 11:25:24 +0530

    feat: Implement user login with JWT authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,13 @@
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs';
+
+import jwt from 'jsonwebtoken';
+
 /**
  * @desc    Register a new user
  * @route   POST /api/users/register
  * @access  Public
  */
-
 export const registerUser = async (req, res, next) => {
     try{
         const { name, email, password}=req.body;
@@ -40,6 +42,43 @@ export const registerUser = async (req, res, next) => {
             res.status(400).json({message: "Invalid user data"});
         }
     } catch (error) {
+        next(error);
+    }
+};
+
+/**
+ * @desc    Authenticate a user & get token
+ * @route   POST /api/users/login
+ * @access  Public
+ */
+export const loginUser = async (req,res,next) => {
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password) {
+            return res.staus(400).json({message: 'Please enter all fields'});
+        }
+
+        const user = await User.findOne({ email });
+
+        if(user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign(
+                { userId: user._id },
+                'your_jwt_secret_key',
+                { expiresIn: '30d'}
+            );
+
+            res.status(200).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                token: token,
+            });
+        } else {
+            res.status(401).json({message: 'Invalid credentials'});
+        }
+    } catch(error) {
         next(error);
     }
 };
