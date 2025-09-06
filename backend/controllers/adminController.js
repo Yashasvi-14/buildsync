02f0a199844573800c306fcc89e76fbf635f59be
Yashasvi-14 <Yashasvi-14@users.noreply.github.com>
2025-09-07 02:12:09 +0530

    feat: Implement delete user endpoint for admin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import Flat from "../models/flatModel.js";
 
 /**
  * @desc    Get all users
@@ -14,3 +15,31 @@ export const getAllUsers = async(req, res, next) => {
     }
 };
 
+/**
+ * @desc    Delete a user
+ * @route   DELETE /api/admin/users/:userId
+ * @access  Private (Admin only)
+ */
+export const deleteUser = async (req, res, next) => {
+    try{
+        const user = await User.findById(req.params.userId);
+
+        if(!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+
+        if(user._id.toString() === req.user._id.toString()) {
+            return res.status(400).json({message: 'Admin cannot delete themselves'});
+        }
+
+        if(user.role === 'resident') {
+            await Flat.findOneAndUpdate({resident: user._id}, {resident: null});
+        }
+
+        await User.findByIdAndDelete(req.params.userId);
+
+        res.status(200).json({message:'User deleted successfully'});
+    } catch (error) {
+        next(error);
+    }
+};
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { getAllUsers } from '../controllers/adminController.js';
+import { getAllUsers, deleteUser} from '../controllers/adminController.js';
 import { protect, authorize } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/users', protect, authorize('admin'), getAllUsers);
+router
+    .route('/users')
+    .get(protect, authorize('admin'), getAllUsers);
+
+router
+    .route('/users/:userId')
+    .delete(protect, authorize('admin'), deleteUser);
 
 export default router;
