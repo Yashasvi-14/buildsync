5939e2bbc746357f3b6b8ff5065797a35a23931d
Yashasvi-14 <Yashasvi-14@users.noreply.github.com>
2025-12-23 20:16:05 +0530

    feat(complaint): expose complaint list,update and assign routes

--- backend/routes/complaintRoutes.js
+++ backend/routes/complaintRoutes.js
@@ -1,9 +1,21 @@
 import express from 'express';
-import { raiseComplaint, getComplaints } from '../controllers/complaintController.js';
+import { raiseComplaint, getComplaints, updateComplaintStatus, assignComplaintToStaff} from '../controllers/complaintController.js';
 import { protect, authorize } from '../middleware/authMiddleware.js';
 
 const router = express.Router({ mergeParams: true});
 
 router.post('/', protect, authorize('resident'), raiseComplaint);
+
+router.get('/', protect, getComplaints);
+
+router.put('/:complaintId', protect, authorize('admin', 'manager'), updateComplaintStatus);
+
+router.put(
+  '/:complaintId/assign',
+  protect,
+  authorize('admin', 'manager'),
+  assignComplaintToStaff
+);
+
 
 export default router;
