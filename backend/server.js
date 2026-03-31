105bbd60a313887b86773186d3c01357495fd728
Yashasvi-14 <Yashasvi-14@users.noreply.github.com>
2026-04-01 00:46:34 +0530

    fix(cors):allow multiple origins including vercel

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,20 @@ import paymentRoutes from './routes/paymentRoutes.js';
 
 const app=express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL
+];
+
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
 
 app.use(express.json());
